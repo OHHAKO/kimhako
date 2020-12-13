fb16dc929ddd7dea7a19db994a9b31584d052500
kimHako <l36h7129@gmail.com>
2020-12-13 15:28:59 +0900

    docs: Nextjs 포스트 수정

--- hey.sh
+++ hey.sh
@@ -1,8 +1,8 @@
 touch ./_posts/2020-nn-nn-blah-post.md
 echo "---
 layout: post
-title: "[blah] blah"
-excerpt: "blah"
+title: \"[blah] blah\"
+excerpt: \"blah\"
 categories: [blah]
 comments: true
 ---" > ./_posts/2020-nn-nn-blah-post.md
